88c7643c1609334519a025c0f3260c750870c277
roundseven <oneroundseven@gmail.com>
2017-12-04 20:31:01 +0800

    koa-ice init

---
+++ setting.js
@@ -0,0 +1,19 @@
+module.exports = {
+    port : '9001', // 应用端口
+    logPath : '', // 设置日志存放位置
+    debug : 'true', // 是否开启调试模式，不开启默认错误日志会存储到日志文件中，开启后会在控制台再输出一份
+
+    static: {
+        port: '',
+        path: ''
+    },
+
+    mail : {
+        host : 'mail3.focuschina.com', // default : 127.0.0.1
+        port : null, // default : 25
+        auth: {
+            user: 'sheny@made-in-china.com',
+            pass: '15531685'
+        }
+    }
+};
