3f98e516d09cd0b76e265b486988bd4f5a98ae9f
roundseven <oneroundseven@gmail.com>
2017-12-12 00:18:16 +0800

    logger review

--- modules/domi/index.js
+++ modules/domi/index.js
@@ -5,30 +5,51 @@
  * @author oneroundseven@gmail.com
  */
 
-const hosts = require('./hosts');
 const logger = require('../logger');
-
-
-//const domi = require('domi');
-
-let path = require('path');
-
-let staticMIME = ['css', 'js', 'gif', 'png', 'html', 'jpeg', 'jpg', 'json', 'pdf', 'swf', 'txt', 'wav', 'wma', 'wmv', 'xml', 'woff', 'ttf', 'svg', 'eot', 'ico'];
-
-let ext;
+const proxy = require('../setting').proxy;
+const {URL} = require('url');
+const path = require('path');
+const staticMIME = ['css', 'js', 'gif', 'png', 'html', 'jpeg', 'jpg', 'json', 'pdf', 'swf', 'txt', 'wav', 'wma', 'wmv', 'xml', 'woff', 'ttf', 'svg', 'eot', 'ico'];
 
 function domiAction() {
     return async (ctx, next)=> {
-        ext = path.extname(ctx.req.url).substring(1);
-        logger.visit(ctx.request.ip + ':' + ctx.req.url);
+        let ext = path.extname(ctx.req.url).substring(1);
+        let myURL = new URL(ctx.request.href);
+        let result = match(myURL.host, myURL.path);
 
-        if (staticMIME.indexOf(ext) === -1 && hosts(ctx.request.href)) {
-            // domi action
+        logger.info(ctx.request.ip + ':' + ctx.req.url);
+
+        if (staticMIME.indexOf(ext) === -1 && result) {
+            // domi action domi(result)
             await asynTest(ctx);
         } else {
             await next();
         }
     }
+}
+
+function match(domain, path) {
+    let matchPath;
+    let result;
+    let app;
+
+    if (!proxy || proxy.length === 0) {
+        return result;
+    }
+
+    for (var i = 0; i < proxy.length; i++) {
+        app = proxy[i];
+
+        if (app.domain === domain && app.filter) {
+            matchPath = path.match(new RegExp(app.filter));
+            if (matchPath) {
+                result = proxy[i];
+                break;
+            }
+        }
+    }
+
+    return result;
 }
 
 function asynTest(ctx) {
--- modules/logger/index.js
+++ modules/logger/index.js
@@ -36,61 +36,14 @@ LOG_TYPE.map((item, index)=> {
     }
 });
 
-function fsName(type) {
-    var d = new Date();
-    var m = d.getMonth() + 1;
-
-    return d.getFullYear() + '_' + (m < 10 ? ('0' + m) : m) + '_' +
-        (d.getDate() < 10 ? ('0'+ d.getDate()) : d.getDate());
-}
-
-function formatDate(fmt, dateStr) {
-    var d = (dateStr ? new Date(dateStr) : new Date());
-    var tmp = fmt;
-
-    var o = {
-        "M+": d.getMonth() + 1, //月份
-        "d+": d.getDate(), //日
-        "h+": d.getHours(), //小时
-        "m+": d.getMinutes(), //分
-        "s+": d.getSeconds(), //秒
-        "q+": Math.floor((d.getMonth() + 3) / 3), //季度
-        "S": d.getMilliseconds() //毫秒
-    };
-
-    if (/(y+)/.test(tmp)) tmp = tmp.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o) {
-        if (new RegExp("(" + k + ")").test(tmp)) {
-            tmp = tmp.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        }
-    }
-    return tmp;
-}
-
-function formatMessage(msg) {
-    //return (global.IP ? (global.IP + ' ') : '') + formatDate('yyyy-MM-dd hh:mm:ss') + ' ' + msg + '\r\n';
-}
-
 module.exports = {
-    visit: (msg)=> {
-        let filePath = logPath + '/visiting.log';
-        fs.appendFile(filePath, formatDate('yyyy-MM-dd hh:mm:ss') + msg + '\r\n', (err)=> {
-            if (err) throw err;
-        });
-    },
-    trace: ()=> {
-
-    },
     info: (msg)=> {
-        logger.info('test hello');
+        logger.info(msg);
     },
-    debug: ()=> {
-
-    },
-    error: ()=> {
-
+    error: (msg)=> {
+        logger.warn(msg);
     },
     warn: (msg)=> {
-
+        logger.warn(msg);
     }
 };
