246ad4cf8fcae05bf96fec18635334110fea0d54
roundseven <oneroundseven@gmail.com>
2017-12-12 16:09:07 +0800

    blacklist filter

---
+++ modules/blackList/index.js
@@ -0,0 +1,58 @@
+// Copyright 2017 FOCUS Inc.All Rights Reserved.
+
+/**
+ * @fileOverview koa-ice
+ * @author oneroundseven@gmail.com
+ */
+
+let black_ips = require('../../blacklist');
+
+function blackList() {
+    return async (ctx, next)=> {
+
+        if (black_ips && black_ips.length > 0) {
+            let ip = ctx.request.ip;
+
+            // localhost
+            if (ip === '::1') {
+                await next();
+                return;
+            }
+
+            // ipv6
+            ip = ip.replace('::ffff:', '');
+
+            let tmp, index;
+            for (let i = 0; i < black_ips.length; i++) {
+                if (ip === black_ips[i]) {
+                    ctx.status = 403;
+                    ctx.body = 'No Permision';
+                    return;
+                } else {
+
+                    if (black_ips[i].indexOf('*') !== -1) {
+                        tmp = black_ips[i].split('.');
+                        ip = ip.split('.');
+                        index = tmp.indexOf('*');
+
+                        if (index !== -1) {
+                            ip.splice(index, 1);
+                            tmp.splice(index, 1);
+
+                            if (ip.join('.') === tmp.join('.')) {
+                                ctx.status = 403;
+                                ctx.body = 'No Permision';
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            await next();
+        } else {
+            await next();
+        }
+    }
+}
+
+module.exports = blackList;
