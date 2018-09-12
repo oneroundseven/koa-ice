b976294dc6201c52d10e4d6bf51bffdad2377eee
roundseven <oneroundseven@gmail.com>
2018-09-12 09:42:01 +0800

    add test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+// Copyright 2018 FOCUS Inc.All Rights Reserved.
+
+/**
+ * @fileOverview summers-ice
+ * @author oneroundseven@gmail.com
+ */
+
+const summserICE = require('../bin/www');
+
+summserICE.start();
