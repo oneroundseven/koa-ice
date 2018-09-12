85e4f551d6cd62313688c2c0a3bddfed0d586b51
roundseven <oneroundseven@gmail.com>
2018-09-12 15:57:28 +0800

    modify loginfo color

--- src/debug/index.js
+++ src/debug/index.js
@@ -10,9 +10,13 @@ const debug = require('debug');
 const logger = require('../logger');
 
 const warn = debug('SUMMER-ICE:Warning');
+warn.color = 3;
 const info = debug('SUMMER-ICE:Info');
+info.color = 0;
 const error = debug('SUMMER-ICE:Error');
+error.color = 41;
 const compile = debug('SUMMER-ICE:Compile');
+compile.color = 6;
 
 module.exports = {
     warn: (content)=> { warn(content); logger.server(content); },
