18041e786fc962fecdb1f66d8c20605cba4dea6d
roundseven <oneroundseven@gmail.com>
2018-09-18 13:01:55 +0800

    remove log4js

--- src/logger/index.js
+++ src/logger/index.js
@@ -6,10 +6,9 @@
  */
 process.env.DEBUG = 'SUMMER-ICE:* ';
 
-const path = require('path');
 const debugPlug = require('debug');
-const log4js = require('log4js');
-let logger;
+const summerUtil = require('summers-util');
+let logger = summerUtil.getLog();
 
 const warn = debugPlug('SUMMER-ICE:Warning');
 warn.color = 3;
@@ -19,12 +18,6 @@ const error = debugPlug('SUMMER-ICE:Error');
 error.color = 41;
 const debug = debugPlug('SUMMER-ICE:Debug');
 debug.color = 6;
-
-// use log4js as logger 如果环境变量配置了NODE_CONFIG_DIR
-if (process.env.NODE_CONFIG_DIR) {
-    log4js.configure(path.join(process.env.NODE_CONFIG_DIR || "./config", "log4js.json"));
-    logger = log4js.getLogger("summers-ice");
-}
 
 module.exports = {
     warn: (content)=> { logger ? logger.warn(content) : warn(content);},
